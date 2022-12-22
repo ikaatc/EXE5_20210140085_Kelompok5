78c65e36968fdd0ca62bd265a5a0798835966171
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-22 16:18:38 +0700

    add method display in class queue

--- EXE5_20210140085_Kelompok5/Program.cs
+++ EXE5_20210140085_Kelompok5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,6 +47,17 @@ namespace EXE5_20210140085_Kelompok5
             ika = ika.next;
             if (ika == null)
                 tania = null;
+        }
+        public void Display()
+        {
+            if (ika == null)
+            {
+                Console.WriteLine("Queue is Empty!");
+                return;
+            }
+            Node Display;
+            for (Display = ika; Display != null; Display = Display.next)
+                Console.WriteLine(Display.name);
         }
     }
     internal class Program
